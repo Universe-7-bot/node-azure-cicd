579958847ed0a065b6771e7ee80c2b96d0347f90
SOHAN SINGHAMAHAPATRA <sohansinghamahapatra@SOHANs-MacBook-Air.local>
2026-01-11 12:39:18 +0530

    add: / endpoint

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Node app deployed successfully with Azure CI/CD",
+        time: new Date().toISOString()
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
